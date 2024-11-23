4887709e25c812570d42f63921dbbafaeeaf76b3
fanonwalker@gmail.com <your.email@example.com>
2024-11-24 02:52:02 +0300

    paper_thickness 4.py

---
+++ paper_thickness_graph.py
@@ -0,0 +1,26 @@
+# define the initial thickness of the paper
+Thickness = 0.00008 # Thickness in metres
+# creating an empty list to store the thickness values
+thickness_list =[]
+
+# add the initial thickness to the list
+thickness_list.append(Thickness)
+# use a for loop to calculate the thickness for each fold and store it in the list 
+folded_thickness=Thickness
+for _ in range(43):
+    folded_thickness = folded_thickness *2 #Double the thickness of each fold
+    thickness_list.append(folded_thickness)
+# check that the 44 values are stored in the list 
+print("Number of thickness values:", len(thickness_list))
+
+
+print("Thickness values:", thickness_list)
+
+import matplotlib.pyplot as plt
+# plot the thickness balues
+plt.plot(thickness_list, marker='*')
+plt.title("Paper Thickness Growth After Each Fold")
+plt.xlabel("Number of Folds")
+plt.ylabel("Thickness (meters)")
+plt.grid(True)
+plt.show()
